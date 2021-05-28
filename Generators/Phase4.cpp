191192d593f810778300a5b5a8b2a97bb2099625
AthulJoseph <55750689+AthulJoseph27@users.noreply.github.com>
2021-05-28 11:44:24 +0530

    Update Phase4.cpp

--- Generators/Phase4.cpp
+++ Generators/Phase4.cpp
@@ -123,7 +123,7 @@ unsigned long long getStateId(Cube *cube, const vector<unsigned long long> &pow2
 void dfs(int level, string algo, Cube *cube, const vector<unsigned long long> &pow2, const vector<char> &moves)
 {
 
-    if (level == 15)
+    if (level == 15 || mp.size() == 663552)
         return;
 
     int n = algo.size();
@@ -218,11 +218,9 @@ int main()
     vector<char> moves = {'R', 'L', 'U', 'D', 'F', 'B'};
     mp[0] = "";
     vector<unsigned long long> pow2 = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328, 281474976710656, 562949953421312, 1125899906842624, 2251799813685248, 4503599627370496, 9007199254740992, 18014398509481984};
-
-    for (int i = 1; i <= 15; i++)
-    {
-        bfs(i, "", &cube, pow2, moves);
-    }
+    
+    dfs(0,"",&cube,pow2,moves);
+    
     sol_file.open("Phase_4_sol.txt", ios::out);
     state_file.open("Phase_4_states.txt", ios::out);
     cout << "Writing Generator Data on to the file" << endl;
